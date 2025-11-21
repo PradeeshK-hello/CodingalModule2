cc569bdb61cb96c4d007d13d6ad92ae384265828
PradeeshK-hello <sp.pradeeshkumar@gmail.com>
2025-11-21 18:15:34 +0800

    Add files via upload

---
+++ OperationsOnList.py
@@ -0,0 +1,28 @@
+list = ["Apple", "Guava", "Mango", "Banana", "Kiwi"]
+
+print("Length of list:", len(list))
+print("First Element:", list[0])
+print("Last Element:", list[-1])
+
+list.append("Papaya")
+print("Updated List:", list)
+
+list.remove("Guava")
+print("Updated List:", list)
+
+list.sort()
+print("Sorted List:", list)
+
+list.pop(1)
+print("Updated List:", list)
+
+list.reverse()
+print("Reversed List:", list)
+
+print("Multiplication on List:", list*2)
+
+list = list[:4]
+print("Sliced List:", list)
+
+list.clear()
+print("Updated List:", list)
